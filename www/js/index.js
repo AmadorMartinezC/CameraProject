dcb1b91049b31825445e849c104e266710906d7a
Amador-Martinez <ttore.amc@gmail.com>
2021-04-22 18:14:24 +0200

    Agregada la funció de fer foto

--- www/js/index.js
+++ www/js/index.js
@@ -22,4 +22,22 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
+
+	document.getElementById("cameraTakePicture").addEventListener("click", cameraTakePicture);
+}
+
+function cameraTakePicture(){
+	navigator.camera.getPicture(onSuccess, onFail,{
+		quality:50,
+		destinationType: Camera.DestinationType.DATA_URL
+	});
+
+	function onSuccess(imageData){
+		var image = document.getElementById('myImage');
+		image.src = "data:image/jpeg;base64,"+imageData;
+	}
+
+	function onFail(message){
+		alert('Error: '+message);
+	}
 }
